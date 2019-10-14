37acfa778a1d2a2fc7f81a1168cf2a2c85e6a41a
AC <alexander.jacob.clarke.1@gmail.com>
2019-10-14 18:08:27 +0100

    [0.4.0] Step 4 Completed
    Required functionality of additional delimiters delimited by //[delimiter]\n added
    
    Test TestInputOfDelimiter now passes.

--- StringCalculator/Calculator.cs
+++ StringCalculator/Calculator.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace StringCalculator
@@ -12,21 +13,17 @@ namespace StringCalculator
         {
             if (string.IsNullOrEmpty(numbers)) return 0;
 
-            string[] delimiters = new string[] {",","\n"};
-            string[] numbersFromList = numbers.Split(delimiters, 0);
-            int count = 0;
+            string[] delimiters = GetDelimiters(numbers);
 
-            foreach(string number in numbersFromList)
-            {
-                count = count + GetIntegerFromString(number);
-            }
-            return count;
+            string finalList = RemoveDelimiterSpecification(numbers);
+            
+            return CountFromList(finalList, delimiters);
         }
 
         private int GetIntegerFromString(string number)
         {
             int numberToReturn;
-            if(Int32.TryParse(number, out numberToReturn))
+            if (Int32.TryParse(number, out numberToReturn))
             {
                 return numberToReturn;
             }
@@ -34,6 +31,46 @@ namespace StringCalculator
             {
                 throw new Exception("Value was not a number");
             }
+        }
+
+        private string[] GetDelimiters(string numbers)
+        {
+            List<string> delimiters = new List<string> { ",", "\n" };
+
+            if (numbers.StartsWith("//"))
+            {
+                //The inputted delimiter will be split from the list by a new line char
+                string[] inputDelimiterBreaker = new string[] { "\n" };
+                //break the string by the newline char and take the first split
+                string[] test = numbers.Split(inputDelimiterBreaker, 0);
+                string inputDelimiterBase = numbers.Split(inputDelimiterBreaker, 0)[0];
+                delimiters.Add(Regex.Replace(inputDelimiterBase, @"\/\/", ""));
+            }
+
+            string[] finalDelimiterList = delimiters.ToArray<string>();
+            return finalDelimiterList;
+        }
+
+        private string RemoveDelimiterSpecification(string numbers)
+        {
+            if (numbers.StartsWith("//"))
+            {
+                string correctedList = Regex.Replace(numbers, "//(.+?)\n", "");
+                return correctedList;
+            }
+            else return numbers;
+        }
+
+        private int CountFromList(string list, string[] delimiters)
+        {
+            string[] numbersFromList = list.Split(delimiters, 0);
+
+            int count = 0;
+            foreach (string number in numbersFromList)
+            {
+            }
+
+            return count;
         }
     }
 }
