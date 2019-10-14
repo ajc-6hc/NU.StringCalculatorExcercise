7084273506cdb2e988544579a198c1b182aabb42
AC <alexander.jacob.clarke.1@gmail.com>
2019-10-14 15:44:19 +0100

    [0.0.2] Calculator/Test basics
    Added calculator class
    Added Add method to calculator class (functionality tbd)
    Added basic tests (empty, 1 int & 2 int)

---
+++ StringCalculator/Tests/BasicTests.cs
@@ -0,0 +1,46 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringCalculator.Tests
+{
+    [TestFixture]
+    public class BasicTests
+    {
+        Calculator calculator;
+
+        string emptyString;
+        string oneIntegerString;
+        string twoIntegerString;
+
+        [SetUp]
+        public void SetUp()
+        {
+            calculator = new Calculator();
+            emptyString = "";
+            oneIntegerString = "7";
+            twoIntegerString = "6,5";
+        }
+
+        [Test]
+        public void TestEmptyString()
+        {
+            Assert.AreEqual(0, calculator.Add(emptyString));
+        }
+
+        [Test]
+        public void TestOneIntegerString()
+        {
+            Assert.AreEqual(7, calculator.Add(oneIntegerString));
+        }
+
+        [Test]
+        public void TestTwoIntegerString()
+        {
+            Assert.AreEqual(11, calculator.Add(twoIntegerString));
+        }
+    }
+}
